4aa7c6097d693daee4efa901b3083a0b73e9dd9a
Masanori Igarashi <askof7th@gmail.com>
2018-08-15 23:23:35 +0900

    add 10-35, print semaphore limits

---
+++ 10_35_sem_limits.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+#include <semaphore.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    printf("SEM_NSEMS_MAX = %ld, SEM_VALUE_MAX = %ld\n",
+            sysconf(_SC_SEM_NSEMS_MAX), sysconf(_SC_SEM_VALUE_MAX));
+    return 0;
+}
